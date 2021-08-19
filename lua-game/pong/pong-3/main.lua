7e340abc6c999c570fc9d78ecc6017b09383f3d7
Tiago Henrique <tiagohs.dev@gmail.com>
2021-08-19 17:41:25 -0300

    Atualização do pong-3

--- lua-game/pong/pong-3/main.lua
+++ lua-game/pong/pong-3/main.lua
@@ -1,29 +1,28 @@
 --[[
-    GD50 2018
-    Pong Remake
+    Autor: Tiago Henrique
+    tiagohs.dev@gmail.com
 
-    pong-3
-    "The Paddle Update"
+    Exercício do curso de desenvolvimento de jogos
+    CS50 - Instrutor Colton Ogden - cogden@cd50.harvard.edu
 
-    -- Main Program --
+    Originalmente programado Nolan Bushnell e Ted Dabney
+    na forma de console ligado a um monitor movido a fichas,
+    "Pong" foi o primeiro game lucrativo da história dando
+    origem a fundação da empresa Atari, em 1972 e a um novo
+    setor da industria.
 
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
+    O game em duas dimensões, simula tênis de mesa onde dois
+    jogadores têm o objetivo de mandar a bola além da borda do
+    seu oponente. Vence o primeiro a marcar 10 pontos.
 
-    Originally programmed by Atari in 1972. Features two
-    paddles, controlled by players, with the goal of getting
-    the ball past your opponent's edge. First to 10 points wins.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-]]
-
--- push is a library that will allow us to draw our game at a virtual
--- resolution, instead of however large our window is; used to provide
--- a more retro aesthetic
---
+    Esta versão é construída para se parecer mais com o NES do que
+    as máquinas Pong originais ou o Atari 2600 em termos de
+    resolução, embora em widescreen (16: 9) para que fique mais
+    bonito em sistemas modernos.
+    ]] --
+-- push é uma biblioteca que nos permite desenhar o jogo em um ambiente virtual
+-- resolução, em vez do "tamanho" da janela;
+-- usado para uma estética mais retrô
 -- https://github.com/Ulydev/push
 push = require 'push'
 
@@ -33,110 +32,76 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
--- speed at which we will move our paddle; multiplied by dt in update
+-- velocidade com que movemos a raquete; multiplicado por dt na atualização.
 PADDLE_SPEED = 200
 
---[[
-    Runs when the game first starts up, only once; used to initialize the game.
-]]
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    -- more "retro-looking" font object we can use for any text
     smallFont = love.graphics.newFont('font.ttf', 8)
 
-    -- larger font for drawing the score on the screen
     scoreFont = love.graphics.newFont('font.ttf', 32)
 
-    -- set LÖVE2D's active font to the smallFont obect
     love.graphics.setFont(smallFont)
 
-    -- initialize window with virtual resolution
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        resizable = false,
-        vsync = true
-    })
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT,
+                     {fullscreen = false, resizable = false, vsync = true})
 
-    -- initialize score variables, used for rendering on the screen and keeping
-    -- track of the winner
+    -- inicializar as variáveis de pontuação, usadas para renderizar os pontos na tela
+    -- e monitorar o vencedor.
+
     player1Score = 0
     player2Score = 0
 
-    -- paddle positions on the Y axis (they can only move up or down)
+    -- posições da raquete no eixo Y (elas só se movem para cima e para baixo).
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT - 50
 end
 
---[[
-    Runs every frame, with "dt" passed in, our delta in seconds 
-    since the last frame, which LÖVE2D supplies us.
-]]
 function love.update(dt)
-    -- player 1 movement
+    -- movimento do player 1
     if love.keyboard.isDown('w') then
-        -- add negative paddle speed to current Y scaled by deltaTime
+        -- adicionar a velocidade negativa da raquete ao Y atual escalonado por DeltaTime.
         player1Y = player1Y + -PADDLE_SPEED * dt
     elseif love.keyboard.isDown('s') then
-        -- add positive paddle speed to current Y scaled by deltaTime
+        -- adicionar a velocidade positiva da raquete ao Y atual scalonado por DeltaTime.
         player1Y = player1Y + PADDLE_SPEED * dt
     end
 
-    -- player 2 movement
+    -- movimento do player 2 
     if love.keyboard.isDown('up') then
-        -- add negative paddle speed to current Y scaled by deltaTime
+        -- adicionar a velocidade negativa da raquete ao Y atual escalonado por DeltaTime.
         player2Y = player2Y + -PADDLE_SPEED * dt
     elseif love.keyboard.isDown('down') then
-        -- add positive paddle speed to current Y scaled by deltaTime
+        -- adicionar a velocidade positiva da raquete ao Y atual scalonado por DeltaTime.
         player2Y = player2Y + PADDLE_SPEED * dt
     end
 end
 
---[[
-    Keyboard handling, called by LÖVE2D each frame; 
-    passes in the key we pressed so we can access.
-]]
-function love.keypressed(key)
-    -- keys can be accessed by string name
-    if key == 'escape' then
-        -- function LÖVE gives us to terminate application
-        love.event.quit()
-    end
-end
+function love.keypressed(key) if key == 'escape' then love.event.quit() end end
 
---[[
-    Called after update by LÖVE2D, used to draw anything to the screen, 
-    updated or otherwise.
-]]
 function love.draw()
-    -- begin rendering at virtual resolution
     push:apply('start')
 
-    -- clear the screen with a specific color; in this case, a color similar
-    -- to some versions of the original Pong
-    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
-    -- draw welcome text toward the top of the screen
     love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-    -- draw score on the left and right center of the screen
-    -- need to switch font to draw before actually printing
+    -- Desenha a pontuação no centro esquerdo e direito da tela.
+    -- Precisamos mudar a fonte para desenhar antes de realmente imprimir.
     love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, 
-        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
+                        VIRTUAL_HEIGHT / 3)
     love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 3)
+                        VIRTUAL_HEIGHT / 3)
 
-    -- render first paddle (left side), now using the players' Y variable
     love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
-    -- render second paddle (right side)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
-    -- render ball (center)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2,
+                            VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
-    -- end rendering at virtual resolution
     push:apply('end')
 end
