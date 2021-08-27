398861a39ae1ed7a1038575f88808e283656faec
Tiago Henrique <tiagohs.dev@gmail.com>
2021-08-27 14:34:07 -0300

    Conclusão

--- lua-game/pong/pong-final/Ball.lua
+++ lua-game/pong/pong-final/Ball.lua
@@ -1,15 +1,4 @@
---[[
-    -- Ball Class --
-
-    Autor: Tiago Henrique
-    Cs50 GameDev - Instrutor Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Representa uma bola que vai quicar para frente e para trás entre as raquetes
-    e paredes até passar um limite esquerdo ou direito da tela, marcando um ponto
-    para o adversário.
-
-   ]] Ball = Class {}
+Ball = Class {}
 
 function Ball:init(x, y, width, height)
     self.x = x
@@ -17,16 +6,10 @@ function Ball:init(x, y, width, height)
     self.width = width
     self.height = height
 
-    -- essas variáveis são para manter o controle da velocidade em ambos os
-    -- eixos X e Y, uma vez que a bola pode se mover em duas dimensões
     self.dy = math.random(2) == 1 and -100 or 100
     self.dx = math.random(-50, 50)
 end
 
---[[
-    Coloca a bola no meio da tela, com uma velocidade inicial aleatória 
-    em ambos os eixos.
-]]
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2 - 2
     self.y = VIRTUAL_HEIGHT / 2 - 2
@@ -34,9 +17,6 @@ function Ball:reset()
     self.dx = math.random(-50, 50)
 end
 
---[[
-    Simplesmente aplica a velocidade à posição, escalonada por DeltaTime. 
-]]
 function Ball:update(dt)
     self.x = self.x + self.dx * dt
     self.y = self.y + self.dy * dt
